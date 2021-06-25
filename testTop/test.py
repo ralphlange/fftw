5e63ebd54377bff9d754a2a8fbe40b38ef030516
Ralph Lange <ralph.lange@gmx.de>
2021-06-25 14:40:00 +0200

    test: add simple test for aSub support

--- testTop/test.py
+++ testTop/test.py
@@ -78,6 +78,40 @@ class TestFFTW(unittest.TestCase):
         self.assertTrue(np.allclose(result.imag, outi.get()))
         self.assertTrue(np.allclose(result.real, outr.get()))
 
+    def test_1ke_asub_1sine(self):
+        """
+        Test a 1k array with a single sine wave - using aSub
+        """
+        real_is_in = False
+        imag_is_in = False
+
+        def data_callback(pvname=None, **kwargs):
+            nonlocal real_is_in, imag_is_in
+            if pvname.endswith('real'):
+                real_is_in = True
+            elif pvname.endswith('imag'):
+                imag_is_in = True
+
+        data = np.cos(2 * np.pi * np.arange(1024) / 1024) + np.cos(4 * 2 * np.pi * np.arange(1024) / 1024)
+        wintype = PV('A3:wintype')
+        inp = PV('A3:inp-real')
+        outr = PV('A3:out-real', callback=data_callback)
+        outi = PV('A3:out-imag', callback=data_callback)
+        while not all([real_is_in, imag_is_in]):
+            time.sleep(0.001)
+        real_is_in = False
+        imag_is_in = False
+
+        wintype.put('None', wait=True)
+        inp.put(data, wait=True)
+
+        result = np.fft.rfft(data)
+
+        while not all([real_is_in, imag_is_in]):
+            time.sleep(0.001)
+
+        self.assertTrue(np.allclose(result.imag, outi.get()))
+        self.assertTrue(np.allclose(result.real, outr.get()))
 
 if __name__ == '__main__':
     unittest.main()
